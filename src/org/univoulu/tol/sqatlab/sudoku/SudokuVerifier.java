c4b85f1c451b8182de92bed9ad55b68b928557c2
somename <someemail>
2015-10-02 12:21:31 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -1,7 +1,13 @@
 package org.univoulu.tol.sqatlab.sudoku;
 
-public class SudokuVerifier {
+import java.util.ArrayList;
 
+public class SudokuVerifier {
+	
+	//ArrayList<E>
+	String correctString = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+
+	int counter=0;
 	public int verify(String candidateSolution) {
 		
 		// returns 1 if the candidate solution is correct
