83c0733d8b7230ea48701790c747643161cd9d0d
somename <someemail>
2015-10-02 12:37:45 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -8,6 +8,7 @@ public class SudokuVerifier {
 	String correctString = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 
 	int counter=0;
+	
 	public int verify(String candidateSolution) {
 		
 		// returns 1 if the candidate solution is correct
