664956a8e0d022f516cf6933db04645c3344313b
somename <someemail>
2015-10-02 12:30:42 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -9,7 +9,15 @@ public class TestSudokuVerifier {
 	@Test
 	public void test() {
 		SudokuVerifier answer=new SudokuVerifier();
-		answer.verify("4");
+		answer.verifytest("4");
+		
+		assertEquals("4", "4");
+	}
+	
+	@Test
+	public void testCheckValidSodukuSolution() {
+		SudokuVerifier answer=new SudokuVerifier();
+		answer.verifyRuleOne(4);
 		
 		assertEquals("4", "4");
 	}
