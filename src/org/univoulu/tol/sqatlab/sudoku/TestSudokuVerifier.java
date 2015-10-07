d52628d725212164636b1de616b2faf6054a2ada
somename <someemail>
2015-10-07 16:00:46 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -8,6 +8,7 @@ public class TestSudokuVerifier {
 	
 	String correctString = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 	String inncorrectString="123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+	String inncorrectString2="012345067809901230405678891234567789123456678912345567891234456789123345678912234";
 
 
 	@Test
@@ -18,14 +19,6 @@ public class TestSudokuVerifier {
 		assertEquals("0", "0");
 	}
 	
-	
-	@Test
-	public void test() {
-		SudokuVerifier answer=new SudokuVerifier();
-		answer.verifytest("4");
-		
-		assertEquals("4", "4");
-	}
 	
 	@Test
 	public void testCheckValidSodukuSolution() {
