1319e4e94dac9cce6df0cd59537d9f857476a447
somename <someemail>
2015-10-07 15:18:55 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -24,45 +24,45 @@ public class SudokuVerifier {
 		// returns 1 if the candidate solution is correct
 		return 0;
 	}
-	public int verifytest(String candidateSolution) {
-//		int [][] tab = {{4,1,7},{3,6,9}};
-//		System.out.println(tab);
-		
-		// returns 1 if the candidate solution is correct
-		return 4;
-	}
-	
-	public int verifyRuleOne(int i) {
-		
-		if(i>=1 && i<=9)
-		{
-			System.out.println("The first rule is filfilled");
-			return 0;
-			}
-		System.out.println("The first rule is not filfilled. - R1: A cell in a Sudoku game can only store positive digits, i.e. 1...9.");
-		return -1;
-		
-	}
-	public int verifyRuleSecond(int i) {
-		
-		
-		if(answerList.contains(i)){
-			return -2;
-		}else{
-			answerList.add(i);
-			return 0;
-		}
-	
-//		for(int j=0;j<=answerList.size(); j++){
-//			int tmp=answerList.get(j);
-//
-//			System.out.println("tmp "+tmp);
+//	public int verifytest(String candidateSolution) {
+////		int [][] tab = {{4,1,7},{3,6,9}};
+////		System.out.println(tab);
+//		
+//		// returns 1 if the candidate solution is correct
+//		return 4;
+//	}
+//	
+//	public int verifyRuleOne(int i) {
+//		
+//		if(i>=1 && i<=9)
+//		{
+//			System.out.println("The first rule is filfilled");
+//			return 0;
+//			}
+//		System.out.println("The first rule is not filfilled. - R1: A cell in a Sudoku game can only store positive digits, i.e. 1...9.");
+//		return -1;
+//		
+//	}
+//	public int verifyRuleSecond(int i) {
+//		
+//		
+//		if(answerList.contains(i)){
+//			return -2;
+//		}else{
+//			answerList.add(i);
+//			return 0;
 //		}
-		
-		
-		
-		
-	}
+//	
+////		for(int j=0;j<=answerList.size(); j++){
+////			int tmp=answerList.get(j);
+////
+////			System.out.println("tmp "+tmp);
+////		}
+//		
+//		
+//		
+//		
+//	}
 	
 	
 	
