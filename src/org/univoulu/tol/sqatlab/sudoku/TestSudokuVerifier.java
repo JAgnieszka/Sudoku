dba6687ba845f2ad02e03e25de839d4cb9db744e
somename <someemail>
2015-10-07 22:34:48 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -7,8 +7,8 @@ import org.junit.Test;
 public class TestSudokuVerifier {
 	
 	String correctString = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
-	String inncorrectString="123456789912345678891234567789123456678912345567891234456789123345678912234567891";
-	String inncorrectString2="012345067809901230405678891234567789123456678912345567891234456789123345678912234";
+	String incorrectString="123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+	String incorrectString2="012345067809901230405678891234567789123456678912345567891234456789123345678912234";
 
 
 	@Test
@@ -19,9 +19,9 @@ public class TestSudokuVerifier {
 		assertEquals(0, 0);
 	}
 	
-	//RULE 1
+	//**********RULE 1********************
 	@Test
-	public void testCheckValidSodukuSolution() {
+	public void testCheckValidSodukuSolutionCORRECT() {
 		
 		SudokuVerifier answer=new SudokuVerifier();
 		
@@ -29,10 +29,19 @@ public class TestSudokuVerifier {
 		answer.verify(correctString);		
 		assertEquals(0, 0);
 		
-		//bad answer
-		answer.verify(inncorrectString2);		
-		assertEquals(-1, -1);
 	}
+	
+		@Test
+		public void testCheckValidSodukuSolutionINCORRECT() {
+			
+			SudokuVerifier answer=new SudokuVerifier();
+			
+			//bad answer
+			answer.verify(incorrectString2);		
+			assertEquals(-1, -1);
+		}
+		
+	//************************************	
 	
 //	@Test
 //	public void testCheckAllDigitsAppearOnlyOnceInSubGrid() {
