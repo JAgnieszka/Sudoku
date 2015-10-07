0f62f284f16456981060b992691e477886eac743
somename <someemail>
2015-10-07 23:10:36 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -31,9 +31,9 @@ public class SudokuVerifier {
 
 		for (int row = 0; row <= 8; row++) {
 			for (int column = 0; column <= 8; column++) {
-
+				System.out.print(tab[row][column]+" ");
 				if (tab[row][column] >= 1 && tab[row][column] <= 9) {
-					System.out.print(tab[row][column]+" ");
+					
 					System.out.println("The first rule is filfilled");
 					flag = true;
 
