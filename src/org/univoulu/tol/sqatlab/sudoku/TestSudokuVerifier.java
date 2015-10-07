ae90efbfd29f962d228d695599a6a20bb1532b46
somename <someemail>
2015-10-07 23:13:36 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -9,16 +9,6 @@ public class TestSudokuVerifier {
 	String correctString = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 	String incorrectString="123456789912345678891234567789123456678912345567891234456789123345678912234567891";
 	String incorrectString2="012345067809901230405678891234567789123456678912345567891234456789123345678912234";
-
-
-	@Test
-	public void tests() {
-		SudokuVerifier answer=new SudokuVerifier();
-		String result;
-		result=answer.verify(correctString);
-		
-		assertEquals("0", result);
-	}
 	
 	//**********RULE 1********************
 	@Test
@@ -26,8 +16,8 @@ public class TestSudokuVerifier {
 		
 		SudokuVerifier answer=new SudokuVerifier();
 		String result;
-		//good answer
 		
+		//good answer
 		result=answer.verify(correctString);		
 		assertEquals("0", result);
 		
