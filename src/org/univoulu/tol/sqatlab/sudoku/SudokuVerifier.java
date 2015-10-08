02edc9b6e5aaf4007ae682a0b1f050ffea3bfd21
somename <someemail>
2015-10-08 11:57:36 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -29,6 +29,12 @@ public class SudokuVerifier {
 			return "-1";
 		}
 
+		// here I check Rule 2
+		result = checkSubGrid(tab);
+		if (result.equals("-2")) {
+			return "-2";
+		}
+
 		// here I check Rule 3
 		result = checkRows(tab);
 		if (result.equals("-3")) {
@@ -43,6 +49,22 @@ public class SudokuVerifier {
 
 		System.out.println("RESULT " + result);
 		return result;
+	}
+
+	private String checkSubGrid(char[][] tab) {
+
+		for (int r = 0, c = 0, n = 3; r < n && c < n && n <= 9; r = r + 3, c = c + 3, n = n + 3) {
+
+			for (int row = r; r < n; r++) {
+				for (int column = c; c < n; c++) {
+					System.out.println(tab[row][column] + " " + "|" + " ");
+				}
+				System.out.println();
+				System.out.println();
+			}
+		}
+
+		return "0";
 	}
 
 	private String checkColumns(char[][] tab2) {
@@ -87,7 +109,7 @@ public class SudokuVerifier {
 		for (int row = 0; row <= 8; row++) {
 			for (int column = 0; column <= 8; column++) {
 
-				System.out.print(tab[row][column] + " ");
+//				System.out.print(tab[row][column] + " ");
 
 				// change from char to int
 				int digit = Character.getNumericValue(tab[row][column]);
