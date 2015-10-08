ac7bdc5bf8a7eebeb4d186b6ebf86932cef27c65
somename <someemail>
2015-10-08 19:37:12 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -70,7 +70,7 @@ public class SudokuVerifier {
 		System.out.println("**************************");
 		for (int r = 3, c = 0, n = 3; c < n && n <= 9; c = c + 3, n = n + 3) {
 			System.out.println("r="+r+" n="+ n+" c="+c);
-			for (int row = r; row < n+3; row++) {
+			for (int row = r; row < 6; row++) {
 				System.out.println("R="+row+" N="+ n);
 				for (int column = c; column < n; column++) {
 					System.out.print(" C="+column+" ");
